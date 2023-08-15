4cb3c40a7cbfdd5305d3cd4aafe04e51b5d356ab
Ulises Martinez <ulimxtz@gmail.com>
2023-08-15 12:36:10 -0600

    Paginación de prodcutos

---
+++ backend/src/Core/Ecommerce.Application/Specifications/Products/ProductForCountingSpecification.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Domain;
+
+namespace Ecommerce.Application.Specifications.Products;
+
+public class ProductForCountingSpecification : BaseSpecification<Product>
+{
+    public ProductForCountingSpecification(ProductSpecificationParams productSpecification) :
+        base(
+            x => (string.IsNullOrEmpty(productSpecification.Search) || x.Nombre!.Contains(productSpecification.Search) 
+                    || x.Descripcion!.Contains(productSpecification.Search))
+            && (!productSpecification.CategoryId.HasValue || x.CategoryId == productSpecification.CategoryId) 
+            && (!productSpecification.PrecioMin.HasValue || x.Precio >= productSpecification.PrecioMin) 
+            && (!productSpecification.PrecioMax.HasValue || x.Precio <= productSpecification.PrecioMax) 
+            && (!productSpecification.Status.HasValue || x.Status == productSpecification.Status)
+        )
+    {
+
+    }
+}
---
+++ backend/src/Core/Ecommerce.Application/Specifications/Products/ProductSpecification.cs
@@ -0,0 +1,63 @@
+﻿using Ecommerce.Domain;
+
+namespace Ecommerce.Application.Specifications.Products;
+
+public class ProductSpecification : BaseSpecification<Product>
+{
+    public ProductSpecification(ProductSpecificationParams productSpecification)
+        :base(
+            x => (string.IsNullOrEmpty(productSpecification.Search) || x.Nombre!.Contains(productSpecification.Search)
+                    || x.Descripcion!.Contains(productSpecification.Search))
+            && (!productSpecification.CategoryId.HasValue || x.CategoryId == productSpecification.CategoryId)
+            && (!productSpecification.PrecioMin.HasValue || x.Precio >= productSpecification.PrecioMin)
+            && (!productSpecification.PrecioMax.HasValue || x.Precio <= productSpecification.PrecioMax)
+            && (!productSpecification.Status.HasValue || x.Status == productSpecification.Status)
+
+         )
+    {
+        AddInclude(p => p.Reviews!);
+        AddInclude(p => p.Images!);
+
+        // APLICAR LA PAGINACION
+        ApplyPaging(productSpecification.PageSize * (productSpecification.PageIndex - 1), productSpecification.PageSize);
+
+        // ORDNENAMIENOT
+        if (!string.IsNullOrEmpty(productSpecification.Sort))
+        {
+            switch (productSpecification.Sort)
+            {
+                case "nombreAsc":
+                    AddOrderBy(p => p.Nombre!);
+                    break;
+
+                case "nombreDesc":
+                    AddOrderByDescending(p => p.Nombre!);
+                    break;
+
+                case "precioAsc":
+                    AddOrderBy(p => p.Precio!);
+                    break;
+
+                case "precioDesc":
+                    AddOrderByDescending(p => p.Precio!);
+                    break;
+
+                case "ratingAsc":
+                    AddOrderBy(p => p.Rating!);
+                    break;
+
+                case "ratingDesc":
+                    AddOrderByDescending(p => p.Rating!);
+                    break;
+
+                default:
+                    AddOrderBy(p => p.CreatedDate!);
+                    break;
+            }
+        }
+        else
+        {
+            AddOrderByDescending(p => p.CreatedDate!);
+        }
+    }
+}
