179148e583f996e4d238faa3e3cffdfe03b736c4
Ulises Martinez <ulimxtz@gmail.com>
2023-08-26 18:28:30 -0600

    Obtener carrito y paginacion de productos

---
+++ backend/src/Api/Controllers/ShoppingCartController.cs
@@ -0,0 +1,35 @@
+﻿using Ecommerce.Application.Features.ShoppingCarts.Queries.GetShoppingCartById;
+using Ecommerce.Application.Features.ShoppingCarts.Vms;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Ecommerce.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+
+        private IMediator _mediator;
+
+        public ShoppingCartController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}", Name ="GetShoppingCart")]
+        [ProducesResponseType(typeof(ShoppingCartVm), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartVm>> GetShoppingCart(Guid id)
+        {
+            var shoppingCartId = id == Guid.Empty ? Guid.NewGuid() : id;
+
+            var query = new GetShoppingCartByIdQuery(shoppingCartId); 
+
+            return await _mediator.Send(query);
+        }
+    }
+}
---
+++ backend/src/Core/Ecommerce.Application/Features/ShoppingCarts/Vms/ShoppingCart.cs
@@ -0,0 +1,59 @@
+﻿namespace Ecommerce.Application.Features.ShoppingCarts.Vms;
+
+public class ShoppingCartVm
+{
+
+    public string? ShoppingCartId { get; set; }
+
+    public List<ShoppingCartItemVm>? ShoppingCartItems { get; set; }
+
+    // OBTENER EL TOTAL DE TODOS LOS PRODUCTOS CON SUS CANTIDADES
+    public decimal Total
+    {
+        get
+        {
+            return Math.Round(
+                ShoppingCartItems!.Sum(x => x.Precio * x.Cantidad) +
+                ShoppingCartItems!.Sum(x => x.Precio * x.Cantidad) * Convert.ToDecimal(0.18) +
+                ((ShoppingCartItems!.Sum(x => x.Precio * x.Cantidad)) < 100 ? 10 : 25), 2
+            );
+        }
+
+        set { }
+    }
+
+    public int Cantidad
+    {
+        get
+        {
+            return ShoppingCartItems!.Sum(x => x.Cantidad);
+        }
+        set { }
+    }
+
+    public int SubTotal
+    {
+        get
+        {
+            return (int)Math.Round(ShoppingCartItems!.Sum(x => x.Precio * x.Cantidad), 2);
+        }
+    }
+
+    public int Impuesto
+    {
+        get
+        {
+            return (int)Math.Round(((ShoppingCartItems!.Sum(x => x.Precio * x.Cantidad)) * Convert.ToDecimal(0.18)), 2);
+        }
+        set { }
+    }
+
+    public decimal PrecioEnvio
+    {
+        get
+        {
+            return (ShoppingCartItems!.Sum(x => x.Precio * x.Cantidad)) < 100 ? 10 :25;
+        }
+        set { }
+    }
+}
