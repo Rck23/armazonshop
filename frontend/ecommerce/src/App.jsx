079f9d5022ea7c0d3261bf58d0fac3581859587f
Ulises Martinez <ulimxtz@gmail.com>
2023-09-09 20:39:34 -0600

    Inicio del Front end

---
+++ frontend/ecommerce/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/layout/Header"
+import './App.css'
+import Footer from "./components/layout/Footer"
+import Home from "./components/Home"
+function App() {
+
+  return (
+    <>
+    
+    <div className="App">
+
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/ecommerce/src/components/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+const Footer = () => {
+  return (
+    <Fragment>
+
+        <footer className='py-1'>
+            <p className='text-center mt-1'>
+                Armazon Ecommerce -2023
+            </p>
+        </footer>
+    </Fragment>
+  )
+}
+
+export default Footer
